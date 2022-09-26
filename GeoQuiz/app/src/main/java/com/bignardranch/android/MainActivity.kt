8138edb78622bddb19b6eead553100dcdff07675
v43d3rm4k4r <vaedermakar@mail.ru>
2022-09-26 20:22:50 +0300

    Edit GeoQuiz

--- GeoQuiz/app/src/main/java/com/bignardranch/android/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/bignardranch/android/MainActivity.kt
@@ -1,25 +1,39 @@
 package com.bignardranch.android
 
-import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 /**
- * Объект `ViewGroup` - подвид `View`, который содержит и упорядочивает другие виджеты. При этом
- * сам `ViewGroup` контент не отображает. Он распоряжается тем, где отображается содержимое других
- * представлений. Объекты `ViewGroup` ещё часто называют __макетами__.
- *
- * В Макете `activity` по умолчанию `ConstraintLayout` - это `ViewGroup`, отвечающий за
- * расположение своего единственного дочернего элемента, виджета `TextView`.
+ * @see android.widget.Toast.setGravity
  */
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        super.
-        setContentView(R.layout.activity_main)
+        super.setContentView(R.layout.activity_main)
 
-        // Every widget (for example TextView or Button) inherited by View class
-        //var button = Button(Context())
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener {
+            Toast.makeText(
+                this,
+                R.string.correct_toast,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        falseButton.setOnClickListener {
+            Toast.makeText(
+                this,
+                R.string.incorrect_toast,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
