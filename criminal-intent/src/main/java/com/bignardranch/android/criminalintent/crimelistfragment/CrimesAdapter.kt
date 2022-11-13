bcba555fcd936fe9baa923eb759c8c6894dedde5
v43d3rm4k4r <vaedermakar@mail.ru>
2022-11-13 15:19:25 +0300

    Some adapter refactoring

--- criminal-intent/src/main/java/com/bignardranch/android/criminalintent/crimelistfragment/CrimesAdapter.kt
+++ criminal-intent/src/main/java/com/bignardranch/android/criminalintent/crimelistfragment/CrimesAdapter.kt
@@ -4,68 +4,121 @@ import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
+import androidx.annotation.LayoutRes
 import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
 import com.bignardranch.android.criminalintent.R
 import com.bignardranch.android.criminalintent.contracts.Navigator
+import com.bignardranch.android.criminalintent.databinding.ListItemCrimeBinding
+import com.bignardranch.android.criminalintent.databinding.ListItemCrimeSeriousBinding
 import com.bignardranch.android.criminalintent.model.Crime
 
 /**
  * Part of the [CrimeListFragment].
  */
+
 class CrimesAdapter(
     private val host: Navigator,
     private val onItemClicked: (crime: Crime) -> Unit,
     private val onCallPoliceClicked: () -> Unit
-) : RecyclerView.Adapter<CrimesAdapter.CrimeHolder>() {
+) : ListAdapter<Crime, CrimesAdapter.Binder>(ItemCallback), View.OnClickListener {
 
-    private var crimes: List<Crime> = emptyList()
+    var crimes: List<Crime> = emptyList()
+        @SuppressLint("NotifyDataSetChanged")
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
 
-    @SuppressLint("NotifyDataSetChanged")
-    fun submitItems(crimes: List<Crime>) {
-        this.crimes = crimes
-        notifyDataSetChanged()
+//    fun submitItems(crimes: List<Crime>) {
+//        this.crimes = crimes
+//    }
+
+    override fun onClick(view: View) {
+        val crime = view.tag as Crime
+        when (view.id) {
+            R.id.list_item_crime, R.id.list_item_crime_serious -> { onItemClicked(crime); host.onCrimeSelected(crime.id) }
+            R.id.call_the_police_button -> onCallPoliceClicked()
+        }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(viewType, parent, false)
-        return CrimeHolder(view, onItemClicked, onCallPoliceClicked)
+    // This method for setting on click listener...
+    override fun onCreateViewHolder(parent: ViewGroup, @LayoutRes viewType: Int): Binder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(viewType, parent, false)
+
+        view.setOnClickListener(this)
+
+        return when (viewType) {
+            ITEM_CRIME_SERIOUS -> {
+                val binding = ListItemCrimeSeriousBinding.bind(view)
+                binding.callThePoliceButton.setOnClickListener(this)
+                SeriousCrimeHolder(binding)
+            }
+            else -> {
+                val binding = ListItemCrimeBinding.bind(view)
+                CrimeHolder(binding)
+            }
+        }
     }
 
-    override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
+    // ...this one is for other stuff
+    override fun onBindViewHolder(holder: Binder, position: Int) {
         holder.bind(crimes[position])
     }
 
-    override fun getItemViewType(position: Int): Int {
-        return when {
-            !crimes[position].isSolved && crimes[position].requiresPolice -> R.layout.list_item_crime_serious
-            else -> R.layout.list_item_crime
+    override fun getItemViewType(position: Int): Int = when {
+            !crimes[position].isSolved && crimes[position].requiresPolice -> ITEM_CRIME_SERIOUS
+            else -> ITEM_CRIME
         }
-    }
 
     override fun getItemCount(): Int = crimes.size
 
-    inner class CrimeHolder(
-        view: View,
-        private val onItemClicked: (crime: Crime) -> Unit,
-        private val onCallPoliceClicked: () -> Unit
-    ) : RecyclerView.ViewHolder(view) {
-
-        private val titleTextView: TextView = itemView.findViewById(R.id.crime_title_text_view)
-        private val dateTextView: TextView = itemView.findViewById(R.id.crime_date_text_view)
-        private val solvedImageView: ImageView? = itemView.findViewById(R.id.crime_solved_image_view)
-        private val callThePoliceButton: Button? = itemView.findViewById(R.id.call_the_police_button)
-
-        fun bind(crime: Crime) {
-            itemView.setOnClickListener { onItemClicked(crime); host.onCrimeSelected(crime.id) }
-            callThePoliceButton?.setOnClickListener { onCallPoliceClicked() }
-            titleTextView.text = crime.title
-            dateTextView.text = crime.date.toString()
-            solvedImageView?.let { it.isVisible = crime.isSolved }
+    abstract class Binder(open val binding: ViewBinding)
+        : RecyclerView.ViewHolder(binding.root) {
+            abstract fun bind(crime: Crime)
         }
+
+    class CrimeHolder(
+        override val binding: ViewBinding,
+    ) : Binder(binding) {
+
+        override fun bind(crime: Crime) {
+            with(binding as ListItemCrimeBinding) {
+                root.tag = crime
+                crimeTitleTextView.text = crime.title
+                crimeDateTextView.text = crime.date.toString()
+                crimeSolvedImageView.isVisible = crime.isSolved
+            }
+        }
+    }
+
+    class SeriousCrimeHolder(
+        override val binding: ViewBinding,
+    ) : Binder(binding) {
+
+        override fun bind(crime: Crime) {
+            with(binding as ListItemCrimeSeriousBinding) {
+                root.tag = crime
+                crimeTitleTextView.text = crime.title
+                crimeDateTextView.text = crime.date.toString()
+            }
+        }
+    }
+
+    object ItemCallback : DiffUtil.ItemCallback<Crime>() {
+        override fun areItemsTheSame(oldItem: Crime, newItem: Crime): Boolean =
+            oldItem.id == newItem.id
+
+        override fun areContentsTheSame(oldItem: Crime, newItem: Crime): Boolean =
+            oldItem == newItem
+    }
+
+    companion object {
+        @LayoutRes const val ITEM_CRIME = R.layout.list_item_crime
+        @LayoutRes const val ITEM_CRIME_SERIOUS = R.layout.list_item_crime_serious
     }
 }
