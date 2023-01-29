dcf53a13e89db11773dabf71a7dacc1c2c4c8119
v43d3rm4k4r <vaedermakar@mail.ru>
2023-01-29 23:39:52 +0300

    Working on main activity

--- nerd-launcher/src/main/java/com/bignerdranch/android/nerdlauncher/NerdLauncherActivity.kt
+++ nerd-launcher/src/main/java/com/bignerdranch/android/nerdlauncher/NerdLauncherActivity.kt
@@ -1,9 +1,11 @@
 package com.bignerdranch.android.nerdlauncher
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import com.bignerdranch.android.nerdlauncher.databinding.ActivityNerdLauncherBinding
 
 class NerdLauncherActivity : AppCompatActivity() {
@@ -27,9 +29,24 @@ class NerdLauncherActivity : AppCompatActivity() {
             addCategory(Intent.CATEGORY_LAUNCHER)
         }
         val activities = packageManager.queryIntentActivities(startupIntent, 0)
+        activities.sortWith(Comparator { a, b ->
+            String.CASE_INSENSITIVE_ORDER.compare(
+                a.loadLabel(packageManager).toString(),
+                b.loadLabel(packageManager).toString()
+            )
+        })
 
         Log.i(TAG, "Found ${activities.size} activities")
     }
+
+//    private fun resolveActivityFor(view: View, intent: Intent): Boolean {
+//        val resolvedActivity = packageManager.resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY)
+//        if (resolvedActivity == null) {
+//            view.isEnabled = false
+//            return false
+//        }
+//        return true
+//    }
 
     private companion object {
         const val TAG = "NerdLauncherActivity"
